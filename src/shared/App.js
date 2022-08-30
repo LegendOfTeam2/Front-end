fd6f439f3b0d5495a639f314075c4197665ff524
SeoNaru <tjehddnr12345@naver.com>
2022-08-31 01:03:15 +0900

    Create) Userposition 추가 css 작업 완료
    components/Userposition css 작업 완료 header css 분리 완료
    
    이슈없음

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,16 +1,23 @@
-// Page
-import Main from '../pages/Main';
-import SignIn from '../pages/SignIn';
+// React
+import { lazy, Suspense } from "react";
 
+//packages
+import { Routes, Route } from "react-router-dom";
+// Page
+import Main from "../pages/Main";
+import SignIn from "../pages/SignIn";
+const Userposition = lazy(() => import("../pages/Userposition"));
 // Packages
-import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' exact='true' element={<Main />} />
-      <Route path='/signin' exact='true' element={<SignIn />} />
-    </Routes>
+    <Suspense>
+      <Routes>
+        <Route path='/' exact='true' element={<Main />} />
+        <Route path='/signin' exact='true' element={<SignIn />} />
+        <Route path='/Userposition' exact='true' element={<Userposition />} />
+      </Routes>
+    </Suspense>
   );
 }
 
