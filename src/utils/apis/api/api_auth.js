4698656fe208344a1fba0b90113bbdcc3574fe58
lucy-pill <schmidtiana95@gmail.com>
2022-09-30 19:26:10 +0900

    Feat) 해시태그 기능 오류 수정

--- src/utils/apis/api/api_auth.js
+++ src/utils/apis/api/api_auth.js
@@ -9,7 +9,7 @@ import { getCookie } from '../../cookie';
 const SERVER_IP = process.env.REACT_APP_REST_API_IP;
 
 const api_auth = axios.create({
-  baseURL: `http://${SERVER_IP}`,
+  baseURL: `https://${SERVER_IP}`,
   headers: {
     'content-type': 'application/json;charset=UTF-8',
   },
